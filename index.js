67b67d5f950e5b72c13a2292e149935b9e903767
Yoshizawa <76186254+Sumire-Yoshizawa@users.noreply.github.com>
2021-08-12 12:07:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,13 +21,7 @@ client.on("message", message => {
       message.content.includes("@everyone")
     )
       return false;
-    if (message.mentions.has(client.user.id)) {
-      message.channel.send(
-        "My prefix is :  " +
-       [`**${prefix}**`]
-      );
-    }
-     else return;
+    
   });
 });
 //prefix
